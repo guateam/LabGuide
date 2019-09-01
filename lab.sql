88dfcaa4a7ef866f7b7101fc6498a74cbfb24113
hanerx <zhangyu199946@gmail.com>
2019-09-01 21:44:40 +0800

    更新服务器文件

--- lab.sql
+++ lab.sql
@@ -1,83 +1,112 @@
 /*
-Navicat MySQL Data Transfer
+ Navicat Premium Data Transfer
 
-Source Server         : localhost_3306
-Source Server Version : 50717
-Source Host           : localhost:3306
-Source Database       : lab
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 80017
+ Source Host           : localhost:3306
+ Source Schema         : lab
 
-Target Server Type    : MYSQL
-Target Server Version : 50717
-File Encoding         : 65001
+ Target Server Type    : MySQL
+ Target Server Version : 80017
+ File Encoding         : 65001
 
-Date: 2019-05-14 15:42:35
+ Date: 01/09/2019 21:44:06
 */
 
-SET FOREIGN_KEY_CHECKS=0;
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
--- Table structure for `article`
+-- Table structure for article
 -- ----------------------------
 DROP TABLE IF EXISTS `article`;
-CREATE TABLE `article` (
+CREATE TABLE `article`  (
   `ID` int(10) NOT NULL AUTO_INCREMENT,
-  `title` varchar(50) NOT NULL COMMENT '文章名',
-  `content` longtext COMMENT '文章内容',
-  `time` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
-  `author` int(10) NOT NULL DEFAULT '0' COMMENT '作者的ID',
-  `tag` varchar(255) DEFAULT NULL COMMENT '包含的tag,逗号隔开',
-  `changer` int(10) NOT NULL DEFAULT '0',
+  `title` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '文章名',
+  `content` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL COMMENT '文章内容',
+  `time` timestamp(0) NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '修改时间',
+  `author` int(10) NOT NULL DEFAULT 0 COMMENT '作者的ID',
+  `tag` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '包含的tag,逗号隔开',
+  `changer` int(10) NOT NULL DEFAULT 0,
   PRIMARY KEY (`ID`) USING BTREE
-) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
+) ENGINE = MyISAM AUTO_INCREMENT = 75 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Records of article
+-- Table structure for article_tag
 -- ----------------------------
-INSERT INTO `article` VALUES ('5', '测试', '<p><u style=\"background-color: rgb(0, 0, 0);\">测试测测﻿</u></p><ul><li class=\"ql-indent-3\">egwj dnaskbrgpw\'3ejgrb fsa</li></ul><pre class=\"ql-syntax\" spellcheck=\"false\">fefw dsdffeb fadbsbgved\n\n</pre><p>ewbf<img src=\"http://localhost:5000/static/uploads/15577594209e007ee71b07bbfe54d2af648d7f5bc01610ef29.jpg\" width=\"629\">vdsbefgg</p>', '2019-05-14 10:59:43', '1', '15', '2');
+DROP TABLE IF EXISTS `article_tag`;
+CREATE TABLE `article_tag`  (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `description` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `icon` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `tag_type` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `article_id` int(10) NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 21 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Compact;
 
 -- ----------------------------
--- Table structure for `tag`
+-- Table structure for comment
+-- ----------------------------
+DROP TABLE IF EXISTS `comment`;
+CREATE TABLE `comment`  (
+  `ID` int(10) NOT NULL AUTO_INCREMENT,
+  `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL,
+  `article_id` int(10) NOT NULL,
+  `user_id` int(10) NOT NULL,
+  `father` int(10) NULL DEFAULT NULL,
+  `time` timestamp(0) NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP(0),
+  PRIMARY KEY (`ID`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 30 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for history
+-- ----------------------------
+DROP TABLE IF EXISTS `history`;
+CREATE TABLE `history`  (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `article_id` int(10) NOT NULL,
+  `user_id` int(10) NOT NULL,
+  `type` int(2) NOT NULL,
+  `date` timestamp(0) NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP(0),
+  `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 38 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for tag
 -- ----------------------------
 DROP TABLE IF EXISTS `tag`;
-CREATE TABLE `tag` (
+CREATE TABLE `tag`  (
   `ID` int(5) NOT NULL AUTO_INCREMENT,
-  `name` varchar(20) NOT NULL COMMENT 'tag名',
-  `father` int(5) DEFAULT NULL,
+  `name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT 'tag名',
+  `father` int(5) NULL DEFAULT NULL,
   PRIMARY KEY (`ID`) USING BTREE
-) ENGINE=MyISAM AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
+) ENGINE = MyISAM AUTO_INCREMENT = 81 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Records of tag
--- ----------------------------
-INSERT INTO `tag` VALUES ('1', '编译器', null);
-INSERT INTO `tag` VALUES ('2', '前端', null);
-INSERT INTO `tag` VALUES ('3', '后端', null);
-INSERT INTO `tag` VALUES ('4', '进阶', null);
-INSERT INTO `tag` VALUES ('5', 'JQuery', '2');
-INSERT INTO `tag` VALUES ('6', 'Vue', '2');
-INSERT INTO `tag` VALUES ('7', 'VueCli', '6');
-INSERT INTO `tag` VALUES ('8', 'VsCode', '1');
-INSERT INTO `tag` VALUES ('15', '测试', null);
-
--- ----------------------------
--- Table structure for `user`
+-- Table structure for user
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
-CREATE TABLE `user` (
+CREATE TABLE `user`  (
   `ID` int(10) NOT NULL AUTO_INCREMENT,
-  `Snum` varchar(20) NOT NULL COMMENT '学号',
-  `username` varchar(20) DEFAULT NULL COMMENT '账号',
-  `password` varchar(255) DEFAULT NULL COMMENT '密码',
-  `phone` varchar(20) DEFAULT NULL COMMENT '手机号',
-  `group` int(2) DEFAULT NULL COMMENT '用户组',
-  `token` varchar(40) NOT NULL DEFAULT '',
-  `face` varchar(255) DEFAULT NULL COMMENT '人脸图片名',
-  PRIMARY KEY (`ID`,`Snum`) USING BTREE
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
+  `Snum` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '学号',
+  `username` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '账号',
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '密码',
+  `phone` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '手机号',
+  `group` int(2) NULL DEFAULT NULL COMMENT '用户组',
+  `token` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '',
+  `face` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '人脸图片名',
+  `desc` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '签名',
+  `head` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '/api/api/static/default.jpg' COMMENT '头像',
+  PRIMARY KEY (`ID`, `Snum`) USING BTREE
+) ENGINE = MyISAM AUTO_INCREMENT = 68 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Records of user
+-- View structure for comment_info
 -- ----------------------------
-INSERT INTO `user` VALUES ('1', '17H034160322', 'yuncn', 'e63d5a15d31234b160911313fa3bbb63ddcf67653afd66fd83c229222e7dbc03', null, '0', 'n0okrFhwA8xp1iT3ZLfRatMvK', 'JwQkipKR3FBD40u1jEUvX8S9M.bmp');
-INSERT INTO `user` VALUES ('2', '17H034160234', 'hanerx', 'ec847003d2eadc9baf60853e8391e167a292c21f01892fcb8bad0f4af6cd74a7', null, '0', 'B0W4OdabZSJQp1IvmTo26KsNx', 'duG2y8CkUVQA5SgnJi49qW0oR.bmp');
-INSERT INTO `user` VALUES ('3', '17H034160233', null, null, null, '1', '', null);
+DROP VIEW IF EXISTS `comment_info`;
+CREATE ALGORITHM = UNDEFINED DEFINER = `lab`@`%` SQL SECURITY DEFINER VIEW `comment_info` AS select `comment`.`ID` AS `ID`,`comment`.`content` AS `content`,`comment`.`article_id` AS `article_id`,`comment`.`user_id` AS `user_id`,`comment`.`father` AS `father`,`comment`.`time` AS `time`,`user`.`username` AS `username`,`user`.`group` AS `group`,`user`.`head` AS `head`,`user`.`desc` AS `desc` from (`comment` join `user`) where (`comment`.`user_id` = `user`.`ID`);
+
+SET FOREIGN_KEY_CHECKS = 1;
