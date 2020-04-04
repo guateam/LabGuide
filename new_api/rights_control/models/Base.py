712e526f5fd089d84c8f6e739bdcd3aecaa96ece
hanerx <zhangyu199946@gmail.com>
2020-04-04 19:50:33 +0800

    搭建框架

--- new_api/rights_control/models/Base.py
+++ new_api/rights_control/models/Base.py
@@ -1,9 +1,17 @@
+from flask import request
+
+from new_api.db import database
+from new_api.util.def_methods import get_user_id
+
+
 class Base:
-    def __init__(self):
+    def __init__(self, right_id):
         """
         所有权限类的基类，用于抽象化权限
         """
-        pass
+        self.id = right_id
+        self.token = request.form['token'] if request.method == 'POST' else request.values.get('token')
+        self.user_id = get_user_id(token=self.token)
 
     def check(self) -> bool:
         """
@@ -11,3 +19,15 @@ class Base:
         :return: 权限检查是否通过
         """
         pass
+
+    def get_rights_by_token(self) -> list:
+        """
+        通过token来获取对应用户的所有基础权限
+        :return: 权限列表
+        """
+        data = []
+        rights = database.get('Rights', [database.get_model('Rights').user_id == self.user_id])
+        for item in rights:
+            if item.type == 0:
+                data.append()
+        return []
--- new_api/rights_control/models/ReadArticle.py
+++ new_api/rights_control/models/ReadArticle.py
@@ -1,12 +1,13 @@
 from flask import request
 
-from models import Base
+from new_api.rights_control.models.Base import Base
 
 
 class ReadArticle(Base):
     def __init__(self, article_id):
+        super(ReadArticle, self).__init__(0)
         self.article_id = article_id
-        self.token = request.form['token'] if request.method == 'POST' else request.values.get('token')
 
     def check(self) -> bool:
+
         return True
