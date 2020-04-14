27446869b15afda6deabdcfb77c528bbd75b82c2
hanerx <zhangyu199946@gmail.com>
2020-04-14 13:17:40 +0800

    完成所有旧API的权限系统对接

--- new_api/api/right.py
+++ new_api/api/right.py
@@ -2,10 +2,11 @@ from flasgger import swag_from
 from flask import Blueprint, request
 
 from new_api.db import database
+from new_api.rights_control.models.Base import Base
 from new_api.rights_control.models.RightControl import RightControl
 from new_api.rights_control.rights_control import right_required
 from new_api.util.def_methods import reply_json, login_required
-from new_api.util.right_methods import check_rights, get_right_group_dict
+from new_api.util.right_methods import get_right_group_dict
 from new_api.util.user_action_methods import record_user_action
 
 right = Blueprint('right', __name__)
@@ -20,7 +21,9 @@ def set_right():
     right_id = request.form['right']
     user_id = request.form['user_id']
     target = request.form['target']
-    if database.add('UserRight', {'user_right': right_id, 'user_id': user_id, 'target': target}):
+    right_type = request.form['right_type']
+    if database.add('UserRight',
+                    {'user_right': right_id, 'user_id': user_id, 'target': target, 'right_type': right_type}):
         record_user_action(token=token, code=0, target=user_id)
         return reply_json(1)
     return reply_json(-1)
@@ -34,8 +37,11 @@ def delete_right():
     token = request.form['token']
     right_id = request.form['right']
     user_id = request.form['user_id']
+    right_type = request.form['right_type']
     if database.delete('UserRight', [database.get_model('UserRight').user_right == right_id,
-                                     database.get_model('UserRight').user_id == user_id]):
+                                     database.get_model('UserRight').user_id == user_id,
+                                     database.get_model('UserRight').right_type == right_type,
+                                     ]):
         record_user_action(token=token, code=1, target=user_id)
         return reply_json(1)
     return reply_json(-1)
@@ -49,12 +55,13 @@ def get_right():
     获取权限是否存在
     :return:
     """
-    token = request.values.get('token')
     right_id = request.values.get('right_id')
     target = request.values.get('target')
-    if check_rights(token=token, right=int(right_id), target=target if target else None):
-        return reply_json(1)
-    return reply_json(-2)
+    if not target:
+        target = None
+    base = Base(right_id=right_id)
+    check_model = {'right': base.id, 'right_type': 0, 'target': target}
+    return reply_json(1) if check_model in base.get_rights_by_token() else reply_json(-2)
 
 
 @right.route('/get_right_group')
